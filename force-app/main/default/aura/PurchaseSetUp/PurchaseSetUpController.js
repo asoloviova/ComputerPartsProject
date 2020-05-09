6890faf83c2956e778c56f98ab7955e76072b040
Alexandra <s.soloviovaa@gmail.com>
2020-05-09 22:40:53 +0300

    Contact picklist field adjusted

--- force-app/main/default/aura/PurchaseSetUp/PurchaseSetUpController.js
+++ force-app/main/default/aura/PurchaseSetUp/PurchaseSetUpController.js
@@ -1,5 +1,11 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doInit: function (component, event, helper) {
+        var action = component.get("c.getContactsPicklist");
+        action.setCallback(this, function (response) {
+            var allValues = response.getReturnValue();
+            console.log('allValues -- >> ' + allValues);
+            component.set("v.ContactPick", allValues);
+        });
+        $A.enqueueAction(action);
     }
 })
