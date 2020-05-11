1f960f4ad4c4eb3109a1f507a6582cb4581756cd
Alexandra <s.soloviovaa@gmail.com>
2020-05-11 10:02:52 +0300

    picklist in addItems component

--- force-app/main/default/aura/PurchaseSetUp/PurchaseSetUpController.js
+++ force-app/main/default/aura/PurchaseSetUp/PurchaseSetUpController.js
@@ -3,7 +3,6 @@
         var action = component.get("c.getContactsPicklist");
         action.setCallback(this, function (response) {
             var allValues = response.getReturnValue();
-            console.log('allValues -- >> ' + allValues);
             component.set("v.ContactPick", allValues);
         });
         $A.enqueueAction(action);
--- force-app/main/default/aura/addItems/addItemsController.js
+++ force-app/main/default/aura/addItems/addItemsController.js
@@ -1,5 +1,10 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doInit: function (component, event, helper) {
+        var action = component.get("c.getItemsPicklist");
+        action.setCallback(this, function (response) {
+            var allValues = response.getReturnValue();
+            component.set("v.ItemList", allValues);
+        });
+        $A.enqueueAction(action);
     }
 })
