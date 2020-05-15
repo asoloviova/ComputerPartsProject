2578a5856e1e1b11913649e299559ee676457704
Alexandra <s.soloviovaa@gmail.com>
2020-05-15 20:45:09 +0300

    Custom setting usage for discount

--- force-app/main/default/aura/addItems/addItemsController.js
+++ force-app/main/default/aura/addItems/addItemsController.js
@@ -13,7 +13,6 @@
     },
     removeDeletedRow: function (component, event, helper) {
         var index = event.getParam("indexVar");
-        console.log(index);
         var ItemRows = component.get("v.ItemRows");
         ItemRows.splice(index, 1);
         component.set("v.ItemRows", ItemRows);
--- force-app/main/default/aura/addItemsRow/addItemsRowController.js
+++ force-app/main/default/aura/addItemsRow/addItemsRowController.js
@@ -16,7 +16,6 @@
     uploadDefaultPrice: function (component, event, helper) {
 
         var itmName = component.find("itemPicklist").get("v.value");
-        console.log(itmName);
         var action = component.get("c.getItemsPrice");
         action.setParams({ name: itmName });
         action.setCallback(this, function (response) {
@@ -30,4 +29,19 @@
         var evt = $A.get("e.c:DeleteRowEvent");
         evt.setParams({ "indexVar": component.get("v.RowIndex") }).fire();
     },
+    manDiscountCheck: function (component, event, helper) {
+        var getMaxDisc = component.get("c.getMaxCustomDiscount");
+        var discInput = component.find("manualDiscount").get("v.value");
+        getMaxDisc.setCallback(this, function (response) {
+            var maxDisc = response.getReturnValue();
+            if (discInput > maxDisc) {
+                component.set("v.manDiscount", maxDisc);
+                alert("input manual discount that's less than " + maxDisc + "%");
+            } else {
+                alert("max disc: " + maxDisc + "%");
+            }
+        });
+        $A.enqueueAction(getMaxDisc);
+
+    }
 })
